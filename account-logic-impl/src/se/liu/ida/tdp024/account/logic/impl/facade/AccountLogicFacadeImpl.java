9fb4a0dcd5a8de0b5cbfe2ce44040e126ebd21a9
JockeKv <Jocke@c-134-141.eduroam.liu.se>
2013-09-26 13:08:25 +0200

    Logic kan hämta data från server

--- account-logic-impl/src/se/liu/ida/tdp024/account/logic/impl/facade/AccountLogicFacadeImpl.java
+++ account-logic-impl/src/se/liu/ida/tdp024/account/logic/impl/facade/AccountLogicFacadeImpl.java
@@ -2,6 +2,8 @@ package se.liu.ida.tdp024.account.logic.impl.facade;
 
 import se.liu.ida.tdp024.account.data.api.facade.AccountEntityFacade;
 import se.liu.ida.tdp024.account.logic.api.facade.AccountLogicFacade;
+import se.liu.ida.tdp024.account.util.http.HTTPHelper;
+import se.liu.ida.tdp024.account.util.http.HTTPHelperImpl;
 
 public class AccountLogicFacadeImpl implements AccountLogicFacade {
     
@@ -12,38 +14,53 @@ public class AccountLogicFacadeImpl implements AccountLogicFacade {
     }
     * */
     
+    
+    HTTPHelper helper = new HTTPHelperImpl();
+    
     // PERSON
     
     @Override
     public String listPersons() {
-        return "[\"Anders\", \"Anders mamma\"]";
+        String res = helper.get("http://enterprise-systems.appspot.com/person/list");
+        
+        return res;
     }
 
     @Override
     public String findPersonByKey(String key) {
-        return "Anders";
+        String res = helper.get("http://enterprise-systems.appspot.com/person/find.key", "key", key);
+        
+        return res;
     }
 
     @Override
     public String findPersonByName(String name) {
-        return "AndersNyckel123";
+        String res = helper.get("http://enterprise-systems.appspot.com/person/find.name", "name", name);
+        
+        return res;
     }
 
     // BANK
     
     @Override
     public String listBanks() {
-        return "[\"Lehman Brothers\", \"ViagraBanken\"]";
-    }
+        String res = helper.get("http://enterprise-systems.appspot.com/bank/list");
+        
+        return res;
+        }
 
     @Override
     public String findBankByKey(String key) {
-        return "Lehman Brothers";
+        String res = helper.get("http://enterprise-systems.appspot.com/bank/find.key", "key", key);
+        
+        return res;
     }
 
     @Override
     public String findBankByName(String name) {
-        return "BankOfEvil";
+        String res = helper.get("http://enterprise-systems.appspot.com/bank/find.name", "name", name);
+        
+        return res;
     }
     
 }
