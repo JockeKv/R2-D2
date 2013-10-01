d66d277ca3ca4d1495475ed11a6e50c7595b38f1
JockeKv <Jocke@c-134-47.eduroam.liu.se>
2013-10-01 12:37:21 +0200

    Lagt till funktioner i LOGIC

--- account-logic-api/src/se/liu/ida/tdp024/account/logic/api/facade/AccountLogicFacade.java
+++ account-logic-api/src/se/liu/ida/tdp024/account/logic/api/facade/AccountLogicFacade.java
@@ -1,10 +1,27 @@
 package se.liu.ida.tdp024.account.logic.api.facade;
 
+import se.liu.ida.tdp024.account.data.api.entity.Account;
+
 
 public interface AccountLogicFacade {
     
+    // DATA INTERACTON FUNCTIONS
+    
     String createAccount(String type, String name, String bank);
     
+    String creditAccount(int id, int amount);
+    String debitAccount(int id, int amount);
+    
+    Boolean createTransaction(); // ???
+    
+    Account findAccount(int id);
+    
+    String listAccounts(String name);
+    String listTransactions(int id);
+    
+    
+    
+    // LOGIC FUNCTIONS
     
     // PERSON
     String listPersons();
--- account-logic-impl/src/se/liu/ida/tdp024/account/logic/impl/facade/AccountLogicFacadeImpl.java
+++ account-logic-impl/src/se/liu/ida/tdp024/account/logic/impl/facade/AccountLogicFacadeImpl.java
@@ -1,10 +1,16 @@
 package se.liu.ida.tdp024.account.logic.impl.facade;
 
+import java.util.Date;
+import java.util.List;
 import se.liu.ida.tdp024.account.data.api.entity.Account;
+import se.liu.ida.tdp024.account.data.api.entity.Transaction;
 import se.liu.ida.tdp024.account.data.api.facade.AccountEntityFacade;
+import se.liu.ida.tdp024.account.data.impl.db.entity.TransactionDB;
 import se.liu.ida.tdp024.account.logic.api.facade.AccountLogicFacade;
 import se.liu.ida.tdp024.account.util.http.HTTPHelper;
 import se.liu.ida.tdp024.account.util.http.HTTPHelperImpl;
+import se.liu.ida.tdp024.account.util.json.AccountJsonSerializer;
+import se.liu.ida.tdp024.account.util.json.AccountJsonSerializerImpl;
 
 public class AccountLogicFacadeImpl implements AccountLogicFacade {
     
@@ -14,21 +20,90 @@ public class AccountLogicFacadeImpl implements AccountLogicFacade {
         this.accountEntityFacade = accountEntityFacade;
     }
     
+   // *** DATA INTERACTION FUNCTIONS ***
     
-    public String createAccount(String type, String name, String bank) {
+    AccountJsonSerializer json = new AccountJsonSerializerImpl();
+    
+    @Override
+    public String createAccount(String type, String name, String bank) {            
+            if(!type.equals("CHECK") || !type.equals("SAVINGS"))
+                return "Invalid account-type";
+        
+            String nameKey = findPersonByName(name);
+            if(nameKey.equals("null"))
+                return "Name does not exist";
+            
+            String bankKey = findBankByName(bank);
+            if(bankKey.equals("null"))
+                return "Bank does not exist";
+            
+            if(accountEntityFacade.createAccount(type, nameKey, bankKey) < 0)
+                return "Account creation failed";
+            
+            
+            return "Account successfully created!";
+    }
+
+    @Override
+    public String creditAccount(int id, int amount) {
+        if(findAccount(id) == null)
+            return "No such account";
+        String timeStamp = new Date().toString();
+        Transaction transaction = new TransactionDB("CREDIT", amount);
+        if(amount > 0) {
+            Boolean status = accountEntityFacade.modifyAccount(id, amount);
+            transaction.setStatus(status);
+        }
+        if(accountEntityFacade.insertTransaction(transaction))
+            return transaction.getStatus().toString();
+        
+        return "FAILED :(";
+    }
+
+    @Override
+    public String debitAccount(int id, int amount) {
+        if(findAccount(id) == null)
+            return "No such account";
+        String timeStamp = new Date().toString();
+        Transaction transaction = new TransactionDB("DEBIT", amount);
+        if(amount > 0) {
+            int mod = amount - (amount*2);
+            Boolean status = accountEntityFacade.modifyAccount(id, mod);
+            transaction.setStatus(status);
+        }
+        if(accountEntityFacade.insertTransaction(transaction))
+            return transaction.getStatus().toString();
+        
+        return "FAILED :(";
+    }
+
+    @Override
+    public Boolean createTransaction() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Account findAccount(int id) {
+        return accountEntityFacade.findAccount(id);
+    }
+
+    @Override
+    public String listAccounts(String name) {
         String nameKey = findPersonByName(name);
-        String bankKey = findBankByName(bank);
-        
-        int id = accountEntityFacade.create("CHECK", nameKey, bankKey);
-        
-        Account account = accountEntityFacade.find(id);
-        
-        String res = account.getAccountType()+","+account.getPersonKey()+","+account.getBankKey();
-        
-        return res;
+        if(nameKey.equals("null"))
+            return "Name does not exist";
+        List<Account> list = accountEntityFacade.listAccounts(nameKey);
+        return json.toJson(list);
+    }
+
+    @Override
+    public String listTransactions(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
     
+    // *** LOGIC FUNCTIONS ***
+
     HTTPHelper helper = new HTTPHelperImpl();
     
     // PERSON
@@ -76,5 +151,10 @@ public class AccountLogicFacadeImpl implements AccountLogicFacade {
         
         return res;
     }
+
     
 }
+
+    
+    
+    
